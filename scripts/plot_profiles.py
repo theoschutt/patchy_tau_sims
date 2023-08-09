c6eaa2c8823bd04c6dcf67f728e6fda5808e453a
Theo Schutt <schutt20@stanford.edu>
2023-08-09 16:53:35 -0700

    update plotting script

--- scripts/plot_profiles.py
+++ scripts/plot_profiles.py
@@ -33,8 +33,10 @@ def parse_args():
 
 def plot_signal_curve(data_txt_list, fn):
     fig, ax = plt.subplots(1,1, figsize=(10,5))
+    labels = ['kSZ TI', 'kSZ Sgn']
 
-    labels = [r'$\tau$', 'tSZ TI', 'tSZ Sgn', 'kSZ', 'CIB', 'AdvACT spectrum']
+#     labels = [r'$\tau$ TI', 'tSZ TI', 'kSZ TI', 'CIB TI', 'AdvACT spectrum']
+    # labels = [r'$\tau$ Sgn', 'tSZ Sgn', 'kSZ Sgn', 'CIB Sgn', 'AdvACT spectrum']
     colors = ['k', 'r', 'b', 'orange', 'c']
     lss = ['-', '-', '-', '-', '--']
 
@@ -58,19 +60,56 @@ def plot_signal_curve(data_txt_list, fn):
         t /= areas
         t_err /= areas
 
-        ax.errorbar(r + i*0.03, t, t_err, label=labels[i], c=colors[i], ls=lss[i])
-        ax.set_ylabel(r'Mean $\tau$')
+        ax.errorbar(r, t, t_err, label=labels[i], c=colors[i], ls=lss[i])
+        ax.set_ylabel(r'$\left<\tau\right>$')
         ax.set_xlabel('Radius [arcmin]')
         ax.legend()
-    plt.savefig(fn+'.pdf', metadata=dict(Subject=data_txt_list), dpi=300, bbox_inches='tight')
+    plt.savefig(fn+'.pdf', metadata=dict(Subject=str(data_txt_list)), dpi=300, bbox_inches='tight')
     plt.savefig(fn+'.png', dpi=100, bbox_inches='tight')
 
 
-def plot_noise_curves(data_txt_list):
+def plot_noise_curves(data_txt_list, fn):
     pass
 
-def plot_noise_ratios(data_txt_list):
-    pass
+def plot_signal_ratios(data_txt_list, fn):
+    fig, ax = plt.subplots(1,1, figsize=(10,5))
+
+    labels = [r'$\tau$ TI', 'tSZ TI', 'kSZ TI', 'CIB TI', 'AdvACT spectrum']
+    # labels = [r'$\tau$ Sgn', 'tSZ Sgn', 'kSZ Sgn', 'CIB Sgn', 'AdvACT spectrum']
+    colors = ['k', 'r', 'b', 'orange', 'c']
+    lss = ['-', '-', '-', '-', '--']
+
+    rad2arcmin = 180. * 60. / np.pi
+
+    t_dat = np.genfromtxt(data_txt_list[0])
+    r = t_dat[:,0]
+    tau = t_dat[:,1] * rad2arcmin**2
+    tau_err = t_dat[:,2] * rad2arcmin**2
+
+    # divide by ring area to get mean tau
+    areas = r.copy()
+    for j in range(len(r)):
+        if j == 0:
+            areas[j] = np.pi * r[j]**2
+        else:
+            areas[j] = np.pi * (r[j]**2 - r[j-1]**2)
+    tau /= areas
+    tau_err /= areas
+
+    for i, txt in enumerate(data_txt_list):
+        dat = np.genfromtxt(txt)
+        t = dat[:,1] * rad2arcmin**2
+        t_err = dat[:,2] * rad2arcmin**2
+
+        t /= areas
+        t_err /= areas
+
+        ax.plot(r, t/tau, label=labels[i], c=colors[i], ls=lss[i])
+        ax.set_ylabel(r'$\left<\tau\right>$/$\left<\tau_{\rm true}\right>$')
+        ax.set_xlabel('Radius [arcmin]')
+        ax.legend()
+    plt.savefig(fn+'.pdf', metadata=dict(Subject=str(data_txt_list)), dpi=300, bbox_inches='tight')
+    plt.savefig(fn+'.png', dpi=100, bbox_inches='tight')
 
 def main():
     args = parse_args()
@@ -79,10 +118,10 @@ def main():
         plot_signal_curve(args.data_files, args.outfile)
 
     elif args.plot_type == 'noise':
-        plot_noise_curves(args.data_files)
+        plot_noise_curves(args.data_files, args.outfile)
 
-    elif args.plot_type == 'noise_ratio':
-        plot_noise_ratios(args.data_files)
+    elif args.plot_type == 'tau_ratio':
+        plot_signal_ratios(args.data_files, args.outfile)
 
     else:
         raise ValueError('Invalid plot type specified:', args.plot_type)
